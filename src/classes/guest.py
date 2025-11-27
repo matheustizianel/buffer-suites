2edb114a776e0f3c2cc3186848d45451e312ee05
matheus <matheus9.mts@gmail.com>
2025-11-27 15:10:48 -0500

    staff and cleaner controllers added and tested with success.

--- src/classes/guest.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from src.classes.person import Person
-
-class Guest(Person):
-    def __init__(self, name, email, phone_number = None, address = None, ):
-        super().__init__(name, email, phone_number, address)
-
-    def __str__(self):
-        return super().__str__()
--- src/classes/person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name, email, phone_number = None, address = None):
-        self.name = name
-        self.email = email
-        self.phone_number = phone_number
-        self.address = address
-
-    def __str__(self):
-        return f"Name: {self.name} | Email: {self.email}"
